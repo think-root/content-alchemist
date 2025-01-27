a794e2c00a094ef45b6dc6f94db3af9dfea60ed5
sigmanor <sigmanor@gmail.com>
2025-01-27 19:05:20 +0200

    feat: add middleware to check bearer token for authorization

---
+++ server/middlewares/auth_middleware.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"chappie/config"
+	"net/http"
+	"strings"
+)
+
+func CheckToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header is missing", http.StatusUnauthorized)
+			return
+		}
+
+		tokenParts := strings.Split(authHeader, " ")
+		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+			http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
+			return
+		}
+
+		token := tokenParts[1]
+		if token != config.BEARER_TOKEN {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
