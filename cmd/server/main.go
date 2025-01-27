91ceff78d98c3362bb512961b04397b9ee54d6c5
sigmanor <sigmanor@gmail.com>
2025-01-27 19:04:41 +0200

    feat: initialize server with chi router and required endpoints

---
+++ cmd/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"chappie/config"
+	"chappie/server/middlewares"
+	"chappie/server/routers"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middlewares.CheckToken)
+
+	r.Post("/think-root/api/manual-generate/", routers.ManualGenerate)
+	r.Post("/think-root/api/auto-generate/", routers.AutoGenerate)
+	r.Post("/think-root/api/get-repository/", routers.GetRepository)
+	r.Post("/think-root/api/update-posted/", routers.UpdatePostedStatus)
+
+	log.Printf("Server listen on port %s (app version: %s)\n\n",
+		config.SERVER_PORT, config.APP_VERSION)
+
+	http.ListenAndServe(":"+config.SERVER_PORT, r)
+}
