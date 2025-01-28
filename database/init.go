ee1eee969a91013d4b246b6d95ab2438b84776d9
sigmanor <sigmanor@gmail.com>
2025-01-28 23:16:25 +0200

    chore: remove unused database initialization code

--- database/init.go
+++ database/init.go
@@ -2,9 +2,7 @@ package database
 
 import (
 	"chappie/config"
-	"log"
 
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -24,27 +22,27 @@ func (GithubRepositories) TableName() string {
 }
 
 func init() {
-	var err error
+	// var err error
 
-	dsnWithoutDB := config.DB_CONNECTION + "/?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsnWithoutDB), &gorm.Config{})
-	if err != nil {
-		log.Printf("Error opening MySQL connection: %v", err)
-	}
+	// dsnWithoutDB := config.DB_CONNECTION + "/?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsnWithoutDB), &gorm.Config{})
+	// if err != nil {
+	// 	log.Printf("Error opening MySQL connection: %v", err)
+	// }
 
-	db.Exec("CREATE DATABASE IF NOT EXISTS `think-root`")
+	// db.Exec("CREATE DATABASE IF NOT EXISTS `think-root`")
 
-	DBThinkRoot, err = gorm.Open(mysql.Open(dsnThinkRoot), &gorm.Config{})
-	if err != nil {
-		log.Printf("Error opening think-root database connection: %v", err)
-	}
-	log.Println("Successfully connected to think-root database")
+	// DBThinkRoot, err = gorm.Open(mysql.Open(dsnThinkRoot), &gorm.Config{})
+	// if err != nil {
+	// 	log.Printf("Error opening think-root database connection: %v", err)
+	// }
+	// log.Println("Successfully connected to think-root database")
 
-	if !DBThinkRoot.Migrator().HasTable(&GithubRepositories{}) {
-		err = DBThinkRoot.AutoMigrate(&GithubRepositories{})
-		if err != nil {
-			log.Printf("Error creating github_repositories table: %v", err)
-		}
-		log.Println("Created github_repositories table")
-	}
+	// if !DBThinkRoot.Migrator().HasTable(&GithubRepositories{}) {
+	// 	err = DBThinkRoot.AutoMigrate(&GithubRepositories{})
+	// 	if err != nil {
+	// 		log.Printf("Error creating github_repositories table: %v", err)
+	// 	}
+	// 	log.Println("Created github_repositories table")
+	// }
 }
