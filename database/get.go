81c3156c06719382dec2fda93d09598c57a33126
sigmanor <sigmanor@gmail.com>
2025-03-21 22:43:37 +0200

    feat: enhance GetRepository function to support sorting by date posted, date added, or id with order options

--- database/get.go
+++ database/get.go
@@ -4,16 +4,42 @@ import (
 	"fmt"
 )
 
-func GetRepository(limit int, posted *bool) ([]GithubRepositories, error) {
+func GetRepository(limit int, posted *bool, sortBy string, sortOrder string) ([]GithubRepositories, error) {
 	var repositories []GithubRepositories
 
-	query := DBThinkRoot.Model(&GithubRepositories{}).Order("id DESC")
-	if limit > 0 {
-		query = query.Limit(limit)
-	}
-
+	query := DBThinkRoot.Model(&GithubRepositories{})
+	
 	if posted != nil {
 		query = query.Where("posted = ?", *posted)
+	}
+	
+	orderBy := "id DESC"
+	
+	switch sortBy {
+	case "date_posted":
+		if sortOrder == "ASC" {
+			orderBy = "date_posted IS NULL DESC, date_posted ASC"
+		} else if sortOrder == "DESC" {
+			orderBy = "date_posted IS NULL ASC, date_posted DESC"
+		}
+	case "date_added":
+		if sortOrder == "ASC" {
+			orderBy = "date_added ASC"
+		} else if sortOrder == "DESC" {
+			orderBy = "date_added DESC"
+		}
+	case "id":
+		if sortOrder == "ASC" {
+			orderBy = "id ASC"
+		} else if sortOrder == "DESC" {
+			orderBy = "id DESC"
+		}
+	}
+	
+	query = query.Order(orderBy)
+	
+	if limit > 0 {
+		query = query.Limit(limit)
 	}
 
 	result := query.Find(&repositories)
