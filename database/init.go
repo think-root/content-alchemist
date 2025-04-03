3dd2cf2fb949d4bb50c0eeecdb3fb8d55fb42560
sigmanor <sigmanor@gmail.com>
2025-04-03 14:13:02 +0300

    feat: add indexes and optimize database connection
    - Added indexes to `URL`, `Posted`, `DateAdded`, and `DatePosted` fields in `GithubRepositories` struct
    - Configured GORM with `PrepareStmt`, `SkipDefaultTransaction`, and `Logger` settings
    - Optimized database connection pool settings

--- database/init.go
+++ database/init.go
@@ -7,6 +7,7 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var dsnThinkRoot = config.DB_CONNECTION +
@@ -15,11 +16,11 @@ var DBThinkRoot *gorm.DB
 
 type GithubRepositories struct {
 	ID         int        `gorm:"column:id;primaryKey;autoIncrement"`
-	URL        string     `gorm:"column:url;type:text;not null"`
+	URL        string     `gorm:"column:url;type:text;not null;index"`
 	Text       string     `gorm:"column:text;type:text;not null"`
-	Posted     bool       `gorm:"column:posted;type:tinyint;not null;default:0"`
-	DateAdded  *time.Time `gorm:"column:date_added;type:datetime"`
-	DatePosted *time.Time `gorm:"column:date_posted;type:datetime"`
+	Posted     bool       `gorm:"column:posted;type:tinyint;not null;default:0;index"`
+	DateAdded  *time.Time `gorm:"column:date_added;type:datetime;index"`
+	DatePosted *time.Time `gorm:"column:date_posted;type:datetime;index"`
 }
 
 func (GithubRepositories) TableName() string {
@@ -29,19 +30,36 @@ func (GithubRepositories) TableName() string {
 func init() {
 	var err error
 
+	gormConfig := &gorm.Config{
+		PrepareStmt:            true,
+		SkipDefaultTransaction: true,
+		Logger:                 logger.Default.LogMode(logger.Error),
+	}
+
 	dsnWithoutDB := config.DB_CONNECTION + "/?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsnWithoutDB), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsnWithoutDB), gormConfig)
 	if err != nil {
 		log.Printf("Error opening MySQL connection: %v", err)
 	}
 
 	db.Exec("CREATE DATABASE IF NOT EXISTS `think-root`")
 
-	DBThinkRoot, err = gorm.Open(mysql.Open(dsnThinkRoot), &gorm.Config{})
+	DBThinkRoot, err = gorm.Open(mysql.Open(dsnThinkRoot), gormConfig)
 	if err != nil {
 		log.Printf("Error opening think-root database connection: %v", err)
 	}
-	log.Println("Successfully connected to think-root database")
+
+	sqlDB, err := DBThinkRoot.DB()
+	if err != nil {
+		log.Printf("Error getting DB instance: %v", err)
+	} else {
+		sqlDB.SetMaxOpenConns(25)
+		sqlDB.SetMaxIdleConns(10)
+		sqlDB.SetConnMaxLifetime(time.Hour)
+		sqlDB.SetConnMaxIdleTime(30 * time.Minute)
+	}
+
+	log.Println("Successfully connected to think-root database with optimized connection pool")
 
 	if !DBThinkRoot.Migrator().HasTable(&GithubRepositories{}) {
 		err = DBThinkRoot.AutoMigrate(&GithubRepositories{})
